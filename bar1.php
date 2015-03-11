b29dcdf51ab44cf79f9afa4c63a3ac4ab6129c7d
Sognus <msognus@gmail.com>
2015-03-11 14:50:16 +0100

    Update bar1.php

--- bar1.php
+++ bar1.php
@@ -1,4 +1,9 @@
 <?php
+#Errors
+# Error #1 - Not enough args or negative numbers
+# Error #2 - 2nd arg cannot be lesser than 1st one
+# Error #3 - Integer args cannot be less than zero
+
 if(!isset($_GET["debug"])) header("Content-Type: image/png");
 
 // Nastavení a konstanty
@@ -11,14 +16,20 @@ $e = "";
 
 $a = isset($_GET["a"]) ? $_GET["a"] : -1;
 $b = isset($_GET["b"]) ? $_GET["b"] : -1;
-$e = $a < 0 || $b < 0 ? "Error" : "";
+$e .= $a < 0 || $b < 0 ? "#1" : "";
 
 
 // Načtení, ošetření a vypočet
+$e .= $a > $b ? "#2" : "";
 $x = $a / ($b / 100);
 $x = $x > 100 ? 100 : $x;
-$x = $x < 0 ? 0 : $x;
+$x = $x < 0 && $a < 0 || $b < 0 ? -65535 : $x;
 
+// Any args negative
+$e .= $x == -65535 ? "#3" : "";
+
+
+// Create image
 $img = @imagecreate($width, $height) or die("Error");
 $bgc = imagecolorallocate($img, 0, 0, 0);
 $text_color = imagecolorallocate($img, 255, 255, 255);
@@ -40,6 +51,7 @@ $y_tc = imagecolorallocate($img, 0, 0, 0);
 
 if($e != "")
 {
+  $e = "Errors:".$e;
   imagestring($img, 2, $point * 40, 1, $e, $text_color);
   imagepng($img);
   imagedestroy($img);
