e9d5d2f8ba3ce686e5d52d09064954d958f94784
davidkriz1 <WARSF1@seznam.cz>
2015-03-12 18:08:39 +0100

    Vytvoření stránky pro cvičení
    Vytvoření stránky pro cvičení

---
+++ game/page/training.php
@@ -0,0 +1,65 @@
+<div class="trainingheaddiv">
+</div>
+<div class="trainingdiv">
+  <div class="trainingtypediv">
+    <div class="trainingtypedescdiv">
+      <div class="trainingtypetitlediv">
+    Tvá aktuální síla: ####
+      </div>
+      <div class="trainingtypedescriptiondiv">
+        Síla zvyšuje vaše poškození způsobené zbraněmi
+      </div>
+    </div>
+    <div class="currencydiv">
+      <div class="currencyimagediv">
+        <img src="http://home.spsostrov.cz/~krizda/php/Images/gold.png" class="currencyimage">
+      </div>
+        <span class="goldcurrencyvaluespan">
+          ####
+        </span>
+    </div>
+  </div>
+  <div title="Trénovat" class="trainingbutton"></div>
+</div>
+<div class="trainingdiv">
+  <div class="trainingtypediv">
+    <div class="trainingtypedescdiv">
+      <div class="trainingtypetitlediv">
+    Tvá aktuální obratnost: ####
+      </div>
+      <div class="trainingtypedescriptiondiv">
+        Obratnost zvyšuje vaši obranu
+      </div>
+    </div>
+    <div class="currencydiv">
+      <div class="currencyimagediv">
+        <img src="http://home.spsostrov.cz/~krizda/php/Images/gold.png" class="currencyimage">
+      </div>
+        <span class="goldcurrencyvaluespan">
+          ####
+        </span>
+    </div>
+  </div>
+  <div title="Trénovat" class="trainingbutton"></div>
+</div>
+<div class="trainingdiv">
+  <div class="trainingtypediv">
+    <div class="trainingtypedescdiv">
+      <div class="trainingtypetitlediv">
+    Tvá aktuální odolnost: ####
+      </div>
+      <div class="trainingtypedescriptiondiv">
+        Odolnost zvyšuje vaše body života
+      </div>
+    </div>
+    <div class="currencydiv">
+      <div class="currencyimagediv">
+        <img src="http://home.spsostrov.cz/~krizda/php/Images/gold.png" class="currencyimage">
+      </div>
+        <span class="goldcurrencyvaluespan">
+          ####
+        </span>
+    </div>
+  </div>
+  <div title="Trénovat" class="trainingbutton"></div>
+</div>
