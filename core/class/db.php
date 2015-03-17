d5b216c4f10e96095bec6cc17b1dbc7f909db9e9
Sognus <msognus@gmail.com>
2015-03-17 11:05:56 +0100

    Another update, not last

--- core/class/db.php
+++ core/class/db.php
@@ -35,28 +35,55 @@ Class db
   
   }
   
+<?php
+Class db
+{
+  public static function isPlayerRegistered($link, $username)
+  {
+	 if($ps = mysqli_prepare($link, 'SELECT COUNT(*) FROM game_users WHERE nick = ?'))
+	 {
+		mysqli_stmt_bind_param($ps, 's', $username);
+		mysqli_stmt_execute($ps);
+		mysqli_stmt_bind_result($ps, $int);
+    
+		while (mysqli_stmt_fetch($ps)) 
+		{
+			$output = $int > 0 ? true : false;
+			return $output;
+		}
+	 }
+  }
+  
+  
+  public static function emailExists($link, $email)
+  {
+	 if($ps = mysqli_prepare($link, 'SELECT COUNT(*) FROM game_users WHERE email = ?'))
+	 {
+		mysqli_stmt_bind_param($ps, 's', $email);
+		mysqli_stmt_execute($ps);
+		mysqli_stmt_bind_result($ps, $int);
+    
+		while (mysqli_stmt_fetch($ps)) 
+		{
+			$output = $int > 0 ? true : false;
+			return $output;
+		}    
+  }
+  
+  }
+  
   public static function getPlayer($link, $name)
   {
-	 if($ps = mysqli_prepare($link, 'SELECT * FROM game_users WHERE nick = ?'))
-	 {
-		mysqli_stmt_bind_param($ps, 's', $name);
-		mysqli_stmt_execute($ps);
-    $result = mysqli_stmt_get_result($ps);
-    return mysqli_fetch_assoc($result);  
-  }
-  return false;
+    $name = system::osetri($name);
+    $result = mysqli_query($link, "SELECT * FROM game_users WHERE nick='".$name."'");
+    return mysqli_fetch_assoc($result);
   }
   
   public static function getPlayerById($link, $id)
   {
-	 if($ps = mysqli_prepare($link, 'SELECT * FROM game_users WHERE id = ?'))
-	 {
-		mysqli_stmt_bind_param($ps, 'd', $id);
-		mysqli_stmt_execute($ps);
-    $result = mysqli_stmt_get_result($ps);
-    return mysqli_fetch_assoc($result);  
-  }
-  return false;
+    $id = system::osetri($id);
+    $result = mysqli_query($link, "SELECT * FROM game_users WHERE id='".$id."'");
+    return mysqli_fetch_assoc($result);
   }
   
   public static function updateLastvisit($link, $id)
