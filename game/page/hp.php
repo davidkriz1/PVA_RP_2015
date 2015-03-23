195e57a991bd9d420c23a8461c49e44b7f27312e
Sognus <msognus@gmail.com>
2015-03-23 10:55:34 +0100

    Fixes everywhere

--- game/page/hp.php
+++ game/page/hp.php
@@ -6,7 +6,7 @@ $e = "";
 while($assoc = mysqli_fetch_assoc($result))
 {
   $maxHP = ($assoc["level"] * 15) + 50 + ($assoc["stamina"] * 5);
-  $sql2 = "UPDATE game_users set HP =".$maxHP."";
+  $sql2 = "UPDATE game_users set HP =".$maxHP." WHERE nick=".$assoc["nick"]."";
   $e .= sprintf("Player %s has now %d hp", $assoc["nick"], $maxHP); 
   $e .= "<br />";
   $resut = mysqli_query($link, $sql2);
